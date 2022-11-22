8e1324181fea2f51d4def1be84775126503ab68b
claudia-j-yile <cshao@alumni.risd.edu>
2022-11-22 01:50:51 -0500

    added Queries

--- Develop/server/schemas/resolvers.js
+++ Develop/server/schemas/resolvers.js
@@ -2,10 +2,30 @@ const { User, Book } = require('../models');
 
 const resolvers = {
     Query: {
-      books: async () => {
-        return Book.find().sort({ title: -1 });
+        books: async (parent, { username }) => {
+            const params = username ? { username } : {};
+            return Book.find(params).sort({ title: -1 });
+          },
+    },
+    book: async (parent, { _id }) => {
+        return Thought.findOne({ _id });
+      }, 
+    users: async () => {
+        return User.find()
+          .select('-__v -password')
+          .populate('books');
+      },
+      user: async (parent, { username }) => {
+        return User.findOne({ username })
+          .select('-__v -password')
+          .populate('books');
+      },
+
+      Mutation: {
+        addUser: async () => {
+          
+        }
       }
-    }
   };
   
   module.exports = resolvers;
--- Develop/server/schemas/typeDefs.js
+++ Develop/server/schemas/typeDefs.js
@@ -1,5 +1,6 @@
 // import the gql tagged template function
 const { gql } = require('apollo-server-express');
+const { User, Book } = require('../models');
 
 type Book {
   authors: String
@@ -11,12 +12,29 @@ type Book {
 
 }
 
+type User {
+  username: String
+  email: String
+  password: String
+  savedBooks: [bookSchema]
+}
+
 // create our typeDefs
 const typeDefs = gql`
+
 type Query {
-  books(username: String): [Book]
+  users: [User]
+  user(username: String!): User
+  book(username: String): [Book]
+  book(_id: ID!): Book
+}
+
+type Mutation {
+  login(email: String!, password: String!): User
+  addUser(username: String!, email: String!, password: String!): User
 }
 `;
+
 
 // export the typeDefs
 module.exports = typeDefs;
